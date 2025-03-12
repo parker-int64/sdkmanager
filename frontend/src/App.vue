5a48a7fa1f21d884f4e6a5cab95a15804c1a4d27
parker-int64 <huxiaochuan2011@hotmail.com>
2025-03-12 23:21:59 +0800

    Refactor the home page using tailwind and flowbite.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,12 +2,36 @@
 import { RouterView, RouterLink } from 'vue-router';
 //font
 import "@fontsource/montserrat"
+import { FwbImg, FwbButton } from 'flowbite-vue'
 </script>
 
 <template>
   <div id="container" class="h-full">
-    <div id="content" class="h-screen flex flex-col pt-10 px-10 space-y-10">
-      <router-view />
+    <div id="content" class="h-screen flex flex-col">
+      <div class="px-10 py-8 flex justify-between">
+        <fwb-img
+          alt="flowbite-vue"
+          src="/logo.webp"
+          size="max-w-xs"
+        />
+        <div class="justify-self-end space-x-10">
+          <fwb-button pill square size="lg">
+            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" fill="#FFFFFF">
+              <path d="m370-80-16-128q-13-5-24.5-12T307-235l-119 50L78-375l103-78q-1-7-1-13.5v-27q0-6.5 1-13.5L78-585l110-190 119 50q11-8 23-15t24-12l16-128h220l16 128q13 5 24.5 12t22.5 15l119-50 110 190-103 78q1 7 1 13.5v27q0 6.5-2 13.5l103 78-110 190-118-50q-11 8-23 15t-24 12L590-80H370Zm70-80h79l14-106q31-8 57.5-23.5T639-327l99 41 39-68-86-65q5-14 7-29.5t2-31.5q0-16-2-31.5t-7-29.5l86-65-39-68-99 42q-22-23-48.5-38.5T533-694l-13-106h-79l-14 106q-31 8-57.5 23.5T321-633l-99-41-39 68 86 64q-5 15-7 30t-2 32q0 16 2 31t7 30l-86 65 39 68 99-42q22 23 48.5 38.5T427-266l13 106Zm42-180q58 0 99-41t41-99q0-58-41-99t-99-41q-59 0-99.5 41T342-480q0 58 40.5 99t99.5 41Zm-2-140Z"/>
+            </svg>
+          </fwb-button>
+          <fwb-button pill square size="lg">
+            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" fill="#FFFFFF">
+              <path d="M424-320q0-81 14.5-116.5T500-514q41-36 62.5-62.5T584-637q0-41-27.5-68T480-732q-51 0-77.5 31T365-638l-103-44q21-64 77-111t141-47q105 0 161.5 58.5T698-641q0 50-21.5 85.5T609-475q-49 47-59.5 71.5T539-320H424Zm56 240q-33 0-56.5-23.5T400-160q0-33 23.5-56.5T480-240q33 0 56.5 23.5T560-160q0 33-23.5 56.5T480-80Z"/>
+            </svg>
+          </fwb-button>
+        </div>
+      </div>
+
+      <div class="bg-[#8dc22b] h-full px-20">
+        <router-view class="mx-auto mt-10" />
+      </div>
+
     </div>
   </div>
 </template>
@@ -29,9 +53,6 @@ html {
   overflow-y: auto;
 }
 
-._app {
-  background-color: #fafaee;
-}
 
 /* cancel the default style of router */
 .router-link-active {
--- frontend/src/views/HomePage.vue
+++ frontend/src/views/HomePage.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { ref, onMounted } from 'vue';
+import { FwbCard } from 'flowbite-vue'
 import DeviceSelector from "../components/DeviceSelector.vue"
 import OSSelector from '../components/OSSelector.vue';
 import USBSelector from '../components/USBSelector.vue';
@@ -7,69 +8,18 @@ const step = ref(1)
 </script>
 
 <template>
-    <div class="column" style="height: 100dvh;">
-        <div class="row q-pa-sm" style="height: 20dvh">
-            <q-img src="/logo.webp" style="max-width: 200px; height: 100px;" fit="contain" />
-            <q-space />
-            <div class="q-pa-sm q-gutter-lg">
-                <q-btn round color="primary" class="icon-btn" @click="" icon="settings" />
-                <q-btn round color="primary" class="icon-btn" icon="mdi-help" />
-            </div>
-        </div>
-
-        <div class="q-pa-md op-area">
-            <!-- <q-btn label="Reset" push color="white" text-color="green" @click="step = 1" class="q-mb-md" /> -->
-
-            <q-stepper v-model="step" header-nav ref="stepper" done-color="green" active-color="accent" class="stepper"
-                animated>
-                <q-step :name="1" title="CHOOSE DEVICE" icon="monitor" :done="step > 1" :header-nav="step > 1"
-                    text-weight-regular class="column stepper">
-
-                    <div class="q-gutter-y-md column justify-between flex-center stepper">
-                        <p style="align-self: flex-start;">Select your device.</p>
-                        <DeviceSelector />
-                        <q-separator />
-
-                        <q-stepper-navigation class="row justify-end" style="width: 100%;">
-                            <q-btn @click="() => { done1 = true; step = 2 }" color="primary" label="NEXT" />
-                        </q-stepper-navigation>
-                    </div>
-
-                </q-step>
-
-                <q-step :name="2" title="CHOOSE JETPACK" icon="create_new_folder" :done="step > 2"
-                    :header-nav="step > 2" class="stepper">
-                    <p>Select a JetPack.</p>
-                    <OSSelector />
-                    <q-stepper-navigation class="row justify-between">
-                        <q-btn flat @click="step = 1" color="black" label="BACK" class="q-ml-sm" />
-                        <q-btn @click="() => { done2 = true; step = 3 }" color="primary" label="NEXT" />
-                    </q-stepper-navigation>
-                </q-step>
-
-                <q-step :name="3" title="USB TARGET" icon="usb" :header-nav="step > 3">
-                    <p>Select a USB target.</p>
-                    <USBSelector />
-
-                    <q-stepper-navigation class="row justify-between">
-                        <q-btn flat @click="step = 2" color="black" label="BACK" class="q-ml-sm" />
-                        <q-btn color="primary" @click="done3 = true" label="FLASH" />
-                    </q-stepper-navigation>
-                </q-step>
-            </q-stepper>
-        </div>
-    </div>
+    <fwb-card>
+      <div class="p-5 h-80 w-80">
+        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          Seeed SDK Manager
+        </h5>
+        <p class="font-normal text-gray-700 dark:text-gray-400">
+          Select your device to begin.
+        </p>
+      </div>
+    </fwb-card>
 </template>
 
 <style scoped>
-.op-area {
-    max-width: 800px;
-    height: 80dvh;
-    align-self: center;
-}
 
-.stepper {
-    background-color: #eeefe3;
-    overflow: hidden;
-}
 </style>
